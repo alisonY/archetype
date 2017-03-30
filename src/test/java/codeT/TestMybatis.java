9328549c1daab215c889086e72c7b9dc9e0736a0
elder <416800698@qq.com>
2017-03-30 23:41:35 +0800

    commit

--- src/test/java/codeT/TestMybatis.java
+++ src/test/java/codeT/TestMybatis.java
@@ -16,7 +16,7 @@ import rml.service.MUserService;
 import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class) // = extends SpringJUnit4ClassRunner
-@ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
+@ContextConfiguration(locations = { "classpath:spring.xml","classpath:spring-mvc.xml", "classpath:spring-mybatis.xml" })
 @ActiveProfiles(value="dev")
 public class TestMybatis  extends AbstractJUnit4SpringContextTests {
 
